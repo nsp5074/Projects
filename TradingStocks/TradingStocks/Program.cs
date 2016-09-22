b7daa33940fc52a38be3faf49d9146358ca5a616
Nicholas Pasqua <nickpasqua91@gmail.com>
2016-09-22 14:52:22 -0400

    One more commit for initialization + added code question to main code

--- TradingStocks/TradingStocks/Program.cs
+++ TradingStocks/TradingStocks/Program.cs
@@ -4,6 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+Writing programming interview questions hasn't made me rich. Maybe trading Apple stocks will.
+Suppose we could access yesterday's stock prices as an array, where:
+
+The indices are the time in minutes past trade opening time, which was 9:30am local time.
+The values are the price in dollars of Apple stock at that time.
+So if the stock cost $500 at 10:30am, stockPricesYesterday[60] = 500.
+
+Write an efficient function that takes stockPricesYesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+
+For example:
+
+  var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+
+getMaxProfit(stockPricesYesterday);
+// returns 6 (buying for $5 and selling for $11)
+
+No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
+*/
+
 namespace TradingStocks
 {
     class Program
